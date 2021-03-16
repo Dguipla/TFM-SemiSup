48944e5c4fd95333b903b8c323f713f6905fbe4a
Dguipla <48276338+Dguipla@users.noreply.github.com>
2021-03-16 17:29:30 +0100

    Update SemiSupervisedDataResults.scala
    new class  in orde to create  the package with sbt

--- src/main/scala/org/apache/spark/ml/semisupervised/SemiSupervisedDataResults.scala
+++ src/main/scala/org/apache/spark/ml/semisupervised/SemiSupervisedDataResults.scala
@@ -1,1 +1,12 @@
+package org.apache.spark.ml.semisupervised
+
+class  SemiSupervisedDataResults  {
+
+  var dataLabeledFinal: Long = 0
+  var dataUnDataLabeledFinal: Long = 0
+  var dataLabeledIni: Long = 0
+  var dataUnLabeledIni: Long = 0
+  var iteracionSemiSuper: Int = 0
+
+}
 
